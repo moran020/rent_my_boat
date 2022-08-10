7947e644dc56ee960a724a87bc3a49304c478818
Tashakashkina <tashakashkina@gmail.com>
2022-08-10 04:58:02 +0300

    fixed calendar

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -41,7 +41,8 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(width: 100,  height: 50,),
+          // Заглушка временная
+          const SizedBox(width: 100,  height: 50,),
          ElevatedButton(
             onPressed: () {
               Navigator.pushNamed(
@@ -50,77 +51,75 @@ class _SplashScreenState extends State<SplashScreen> {
               );
             },
             child: const Text('Переход на домашнюю страницу')),
-        SizedBox(width: 100,  height: 100,),
-        // Календарь 
-        Expanded(
-        child: SizedBox(
-          height: 282,
-          width: 288,
-          child: TableCalendar(
-        firstDay: DateTime.utc(2020,10,20),
-        lastDay: DateTime.utc(2025,10,20),
-        focusedDay: DateTime.now(),
-        headerVisible: true,
+            // Заглушка временная
+        const SizedBox(width: 100,  height: 100,),
         
-        shouldFillViewport: false,
-        calendarFormat: format,
-        onFormatChanged: (CalendarFormat _format) {
-              setState(() {
-                format = _format;
-              });
-            },
-            startingDayOfWeek: StartingDayOfWeek.sunday,
-            daysOfWeekVisible: true,
+    //     // Здесь начинается календарь для страницы с анкетой владельца
+    //     Expanded(
+    //     child: SizedBox(
+    //       height: 282,
+    //       width: 288,
+    //       child: TableCalendar(
+    //     firstDay: DateTime.utc(2020,10,20),
+    //     lastDay: DateTime.utc(2025,10,20),
+    //     focusedDay: DateTime.now(),
+    //     headerVisible: true,
+    //     shouldFillViewport: false,
+    //     calendarFormat: format,
+    //     onFormatChanged: (CalendarFormat _format) {
+    //           setState(() {
+    //             format = _format;
+    //           });
+    //         },
+    //         startingDayOfWeek: StartingDayOfWeek.sunday,
+    //         daysOfWeekVisible: true,
 
-            //Смена дней
-            onDaySelected: (DateTime selectDay, DateTime focusDay) {
-              setState(() {
-                selectedDay = selectDay;
-                focusedDay = focusDay;
-              });
-              print(focusedDay);
-            },
-            selectedDayPredicate: (DateTime date) {
-              return isSameDay(selectedDay, date);
-            },
-            eventLoader: _getEventsfromDay,
-         //Шрифты и стиль календаря
-            calendarStyle: CalendarStyle(
-              defaultTextStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400, fontFamily: 'Montserrat-Normal'),
-              isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                color: selectedDate,
-                shape: BoxShape.circle,
-                
-              ),
-              selectedTextStyle: TextStyle(color: Colors.white),
-              todayDecoration: BoxDecoration(
-                color: todaysDate,
-                shape: BoxShape.circle,
-                
-              ),
-              defaultDecoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                
-              ),
-              weekendDecoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                
-              ),
-            ),
-            headerStyle: const HeaderStyle(
-              formatButtonVisible: false,
-              titleCentered: true,
-              formatButtonShowsNext: false,
-              titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, fontFamily: 'Montserrat-Normal'),
-              rightChevronPadding: EdgeInsets.all(10.0),
-              leftChevronPadding: EdgeInsets.all(10.0),
-              leftChevronMargin: EdgeInsets.symmetric(horizontal: 15.0),
-              rightChevronMargin:  EdgeInsets.symmetric(horizontal: 15.0),
-            ),
-          ),
-          
-    ))]),
+    //         //Смена дней
+    //         onDaySelected: (DateTime selectDay, DateTime focusDay) {
+    //           setState(() {
+    //             selectedDay = selectDay;
+    //             focusedDay = focusDay;
+    //           });},
+    //         selectedDayPredicate: (DateTime date) {
+    //           return isSameDay(selectedDay, date);},
+    //         eventLoader: _getEventsfromDay,
+
+    //      //Шрифты и стиль календаря
+    //         calendarStyle: CalendarStyle(
+    //           defaultTextStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400, fontFamily: 'Montserrat-Normal'),
+    //           isTodayHighlighted: true,
+    //           selectedDecoration: BoxDecoration(
+    //             color: selectedDate,
+    //             shape: BoxShape.circle, 
+    //           ),
+    //           selectedTextStyle: TextStyle(color: Colors.white),
+    //           todayDecoration: BoxDecoration(
+    //             color: todaysDate,
+    //             shape: BoxShape.circle,
+    //           ),
+    //           defaultDecoration: const BoxDecoration(
+    //             shape: BoxShape.circle,
+    //           ),
+    //           weekendDecoration: const BoxDecoration(
+    //             shape: BoxShape.circle,
+    //           ),
+    //         ),
+    //         headerStyle: const HeaderStyle(
+    //           formatButtonVisible: false,
+    //           titleCentered: true,
+    //           formatButtonShowsNext: false,
+    //           titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, fontFamily: 'Montserrat-Normal'),
+    //           // Требуется правка расстояние шевронов (должно зависеть от длины строки месяц+год)
+    //           rightChevronPadding: EdgeInsets.all(10.0),
+    //           leftChevronPadding: EdgeInsets.all(10.0),
+    //           leftChevronMargin: EdgeInsets.symmetric(horizontal: 15.0),
+    //           rightChevronMargin:  EdgeInsets.symmetric(horizontal: 15.0),
+    //         ),
+    //       ),   
+    // ),
+    // ),
+    ]
+    ),
     );
   }
 }
