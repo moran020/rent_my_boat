c4c5532e8e8bd438fa5def91b088a804db824190
Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>
2022-08-11 15:53:43 +0300

    test
    Signed-off-by: Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>

--- lib/data/colors.dart
+++ lib/data/colors.dart
@@ -7,4 +7,3 @@ Color inactiveButton = Color.fromRGBO(219, 219, 219, 1);
 Color todaysDate = Color.fromRGBO(254, 213, 1, 1);
 Color selectedDate = Color.fromRGBO(254, 213, 1, 0.7);
 Color basicText = Color.fromARGB(177, 0, 0, 0);
-Color test = Color.fromARGB(177, 0, 0, 0);
