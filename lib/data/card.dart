267118ac8b28648d9b5ce3207be12dda570e6b3a
jestyal <natali.doroshenko14@gmail.com>
2022-08-15 16:38:51 +0500

    Added card json. Added future builder. Fixed card

--- lib/data/card.dart
+++ lib/data/card.dart
@@ -1,26 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
-class Card {
-  int id;
+Future<CardsList> getCardList() async {
+  var jsonData = await rootBundle.loadString('assets/json/card.json');
+  var data = json.decode(jsonData);
+  return CardsList.fromJson(data);
+}
+
+class CardsList {
+  List<CardItem>? cards;
+
+  CardsList({this.cards});
+
+  CardsList.fromJson(Map<String, dynamic> json) {
+    if (json['cards'] != null) {
+      cards = <CardItem>[];
+      json['cards'].forEach((v) {
+        cards!.add(CardItem.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (cards != null) {
+      data['cards'] = cards!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class CardItem {
+  int? id;
   String? name;
   String? description;
   String? price;
-  String? img; // list? //слайдер
+  List<String>? carouselImg;
   bool? isLabel;
   String? labelTitle;
-  Color? labelColor;
 
-  Card({
-    required this.id,
-    required this.name,
-    this.description,
-    required this.price,
-    this.img,
-    this.isLabel,
-    this.labelTitle,
-    this.labelColor,
-  });
+  CardItem(
+      {this.id,
+        this.name,
+        this.description,
+        this.price,
+        this.carouselImg,
+        this.isLabel,
+        this.labelTitle});
 
-// сделать подгрузку из JSON
+  CardItem.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    price = json['price'];
+    carouselImg = json['carouselImg'].cast<String>();
+    isLabel = json['isLabel'];
+    labelTitle = json['labelTitle'];
+  }
 
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['description'] = description;
+    data['price'] = price;
+    data['carouselImg'] = carouselImg;
+    data['isLabel'] = isLabel;
+    data['labelTitle'] = labelTitle;
+    return data;
+  }
 }
--- lib/screens/card/card_screen.dart
+++ lib/screens/card/card_screen.dart
@@ -1,31 +1,8 @@
 import 'package:flutter/material.dart';
+import '../../data/card.dart';
 import '../../data/colors.dart';
 import 'card_detail.dart';
-
-class Card {
-  int id;
-  String? name;
-  String? description;
-  String? price;
-  String? img; // list? //слайдер
-  bool? isLabel;
-  String? labelTitle;
-  Color? labelColor;
-
-  Card({
-    required this.id,
-    required this.name,
-    this.description,
-    required this.price,
-    this.img,
-    this.isLabel,
-    this.labelTitle,
-    this.labelColor,
-  });
-
-// сделать подгрузку из JSON
-
-}
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CardScreen extends StatefulWidget {
   const CardScreen({Key? key}) : super(key: key);
@@ -35,172 +12,132 @@ class CardScreen extends StatefulWidget {
 }
 
 class _CardScreenState extends State<CardScreen> {
-  static List<Card> getCards() {
-    return [
-      Card(
-        id: 0,
-        name: 'Лодка',
-        description: 'лодка лодка',
-        price: '1000',
-        img: 'https://mot63.ru/upload/iblock/c16/c168f833e19dca05c1c662b21aa9b51a.jpg',
-        isLabel: false,
-        labelTitle: '',
-        labelColor: null,
-      ),
-      Card(
-        id: 1,
-        name: 'Яхта',
-        description: 'Яхта Яхта',
-        price: '2000',
-        img: 'https://s0.rbk.ru/v6_top_pics/media/img/1/80/756503489146801.jpg',
-        isLabel: true,
-        labelTitle: 'Скидка',
-        labelColor: todaysDate,
-      ),
-      Card(
-        id: 2,
-        name: 'Бригантина',
-        description: 'Бригантина Бригантина',
-        price: '10000',
-        img: 'https://media.team2.travel/user_image/26869/f60301aeec28f08c00406f935366a3cf.jpg',
-        isLabel: true,
-        labelTitle: 'Скидка',
-        labelColor: todaysDate,
-      ),
-      Card(
-        id: 3,
-        name: 'Катер',
-        description: 'Катер Катер Катер',
-        price: '1000',
-        img: 'https://ufa-lodki.ru/articles/wp-content/uploads/2020/12/2_005-1024x683.jpg',
-        isLabel: true,
-        labelTitle: 'Скидка',
-        labelColor: todaysDate,
-      ),
-    ];
-  }
+  late Future<CardsList> futureData;
 
-  List<Card> card = getCards();
+  @override
+  void initState() {
+    super.initState();
+    futureData = getCardList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   // AppBAR будет???????????????? (название с локализацией)
-      //   backgroundColor: activeButton,
-      //   centerTitle: true,
-      //   leading: IconButton(
-      //     icon: Icon(Icons.arrow_back, color: background),
-      //     onPressed: () {},
-      //   ),
-      //   title: Text('Boats',
-      //       style: TextStyle(
-      //         color: background,
-      //         // fontWeight: FontWeight.w600
-      //       )),
-      //   actions: <Widget>[
-      //     IconButton(
-      //       icon: Icon(Icons.notifications_none, color: background),
-      //       onPressed: () {},
-      //     ),
-      //   ],
-      // ),
       body: Container(
         padding: const EdgeInsets.only(top: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // тут фильтры
+            // TODO: тут фильтры
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-              //Текст с локализацией + количество лодок
-              child: Text("${card.length} boats available", style: const TextStyle(fontSize: 20.0)),
+              //TODO: Текст с локализацией + количество лодок
+              child: Text("58 boats available", style: const TextStyle(fontSize: 20.0)),
             ),
             Expanded(
-              child: ListView.builder(
-                  itemCount: card.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
-                        //navigate to card details
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const CardDetailsScreen(),
-                            ));
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Stack(
-                              children: [
-                                Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.only(
-                                        topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      // Image.asset СЛАЙДЕР!!!!!!!!!!
-                                      // image: NetworkImage(card[index].img.toString()),
-                                      image: NetworkImage(card[index].img.toString()),
-                                    ),
-                                  ),
-                                ),
-                                card[index].isLabel == true
-                                    ? Container(
-                                        margin: const EdgeInsets.all(16.0),
-                                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                                        decoration: BoxDecoration(
-                                          // color: card[index].labelColor.toString(),
-                                          color: todaysDate,
-                                          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                                        ),
-                                        child: Text(
-                                          card[index].labelTitle.toString(),
-                                          style: const TextStyle(color: Colors.white),
-                                        ),
-                                      )
-                                    : Container(),
-                              ],
-                            ),
-                            Container(
-                              // margin: const EdgeInsets.all(16.0),
-                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                              decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(20.0),
-                                    bottomRight: Radius.circular(20.0),
-                                  ),
-                                  boxShadow: [BoxShadow(blurRadius: 5, color: Colors.black45, offset: Offset(0, 3))]),
+              child: FutureBuilder(
+                future: futureData,
+                builder: (BuildContext context, data) {
+                  if (data.hasError) {
+                    return Center(child: Text("${data.error}"));
+                  } else if (data.hasData) {
+                    var cards = data.data as CardsList;
+                    var items = cards.cards as List<CardItem>;
+                    return ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () {
+                              //TODO: передать параметры
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const CardDetailsScreen(),
+                                  ));
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.all(16.0),
                               child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
                                 children: [
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      card[index].name.toString(),
-                                      style: const TextStyle(fontSize: 18),
-                                    ),
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.only(
+                                              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                                          image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            // Image.asset СЛАЙДЕР!!!!!!!!!!
+                                            // image: NetworkImage(card[index].img.toString()),
+                                            image: NetworkImage(
+                                              items[index].carouselImg![0].toString(),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      items[index].isLabel == true
+                                          ? Container(
+                                              margin: const EdgeInsets.all(16.0),
+                                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                              decoration: BoxDecoration(
+                                                // color: card[index].labelColor.toString(),
+                                                color: todaysDate,
+                                                borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                                              ),
+                                              child: Text(
+                                                items[index].labelTitle.toString(),
+                                                style: const TextStyle(color: Colors.white),
+                                              ),
+                                            )
+                                          : Container(),
+                                    ],
                                   ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      "${card[index].price.toString()} руб.",
-                                      // style: const TextStyle(fontSize: 16),
+                                  Container(
+                                    // margin: const EdgeInsets.all(16.0),
+                                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                    decoration: const BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(20.0),
+                                          bottomRight: Radius.circular(20.0),
+                                        ),
+                                        boxShadow: [
+                                          BoxShadow(blurRadius: 5, color: Colors.black45, offset: Offset(0, 3))
+                                        ]),
+                                    child: Column(
+                                      children: [
+                                        Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Text(
+                                            items[index].name.toString(),
+                                            style: const TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                        Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Text(
+                                            "${items[index].price.toString()} руб.",
+                                            // style: const TextStyle(fontSize: 16),
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
-                      ),
+                          );
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                  }),
+                  }
+                },
+              ),
             ),
           ],
         ),
