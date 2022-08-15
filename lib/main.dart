1b1f2f5f3ccfc44c6a138697181ebbd63fe323f9
Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>
2022-08-15 15:44:43 +0300

    Revert "Merge branch 'main' into vero"
    This reverts commit 6fcdb6745010a1170ee1700946cad4e747400aa5, reversing
    changes made to d1e0ba2dd1036002a4329e70a9565447539b8175.
    
    Signed-off-by: Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/home_screen.dart';
 import 'screens/owner_form.dart';
-import  '../data/language.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -12,34 +9,8 @@ void main() {
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-
-  static void setLocale(BuildContext context, Locale newLocale) {
-    _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
-    state!.setLocale(newLocale);
-  }
-}
-
-
-class _MyAppState extends State<MyApp> {
-
-  Locale? _locale;
-
-  setLocale(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    getLocale().then((locale) => {setLocale(locale)});
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,32 +20,11 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         fontFamily: 'Montserrat',
       ),
-      localeResolutionCallback: (deviceLocale, supportedLocales) {
-        _locale ??= deviceLocale;
-        return _locale;
-      },
-      //localizationsDelegates: AppLocalizations.localizationsDelegates,
-      //supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates:  const[
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('ru', ''),
-        Locale('de', ''),
-        Locale('es', ''),
-      ],
       home: const HomeScreen(),
-      initialRoute: '/',
       routes: {
         '/home': (context) => const HomeScreen(),
         '/owner_form': (context) => const OwnerForm(),
       },
-      locale: _locale,
     );
   }
 }
